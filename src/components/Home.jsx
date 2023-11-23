67dd2e43b00c93e44c634eb2c4393831abbdd770
Alan Mandriaza <alan.mandriaza@gmail.com>
2023-11-23 02:56:12 -0300

    barra de busqueda funcionando (basicamente)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home({ registros }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredRegistros = registros.filter(vehiculo => {
+    const term = searchTerm.toLowerCase();
+    return vehiculo.marca.toLowerCase().includes(term) || vehiculo.modelo.toLowerCase().includes(term);
+  });
+
   return (
     <div>
-      <p>Número de Vehículos Registrados: {registros.length}</p>
-      {registros.length > 0 && (
-        <div>
-          <h3>Vehículos Registrados</h3>
-        </div>
+      <input
+        type="text"
+        placeholder="Buscar por Marca o Modelo"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      
+      {searchTerm && (
+        <ul>
+          {filteredRegistros.map(vehiculo => (
+            <li key={vehiculo.servicioNumber}>
+              {`${vehiculo.marca} ${vehiculo.modelo}`}
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   );
 }
+
 export default Home;
