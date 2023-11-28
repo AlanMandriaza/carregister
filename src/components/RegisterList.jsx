a400c792f3de93e5cd18747a49f0890cd67194f9
Alan Mandriaza <alan.mandriaza@gmail.com>
2023-11-27 23:52:07 -0300

    get vehiculo listo

--- src/components/RegisterList.jsx
+++ src/components/RegisterList.jsx
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Requeriments from './Requeriments';
+
 import '../styles/RegisterForm.css';
 import '../styles/RegisterList.css';
 
+function RegisterList() {
+  const [registros, setRegistros] = useState([]);
+  const [currentSelectedVehicle, setCurrentSelectedVehicle] = useState(null);
 
-function RegisterList({ registros, selectedVehicle }) {
-  const [currentSelectedVehicle, setCurrentSelectedVehicle] = useState(selectedVehicle);
-
-  // cargar el vehiculo seleccionado del almacenamiento local cuando el componente se monta
-  useEffect(() => {
-    const storedVehicle = localStorage.getItem('selectedVehicle');
-    if (storedVehicle) {
-      setCurrentSelectedVehicle(JSON.parse(storedVehicle));
-
-      localStorage.removeItem('selectedVehicle');
+  // Función para cargar los registros de vehículos
+  const cargarRegistros = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/vehiculos');
+      setRegistros(response.data);
+      // Si hay vehículos, selecciona el primero por defecto
+      if (response.data.length > 0) {
+        setCurrentSelectedVehicle(response.data[0]);
+      }
+    } catch (error) {
+      console.error('Error al obtener los registros de vehículos:', error);
     }
-  }, []);
+  };
 
+  // Cargar registros al montar el componente
   useEffect(() => {
-    // actualiza el vehiculo seleccionado cuando cambia desde el Navbar
-    setCurrentSelectedVehicle(selectedVehicle);
-  }, [selectedVehicle]);
+    cargarRegistros();
+  }, []);
 
   const handleVehicleClick = (vehiculo) => {
     setCurrentSelectedVehicle(vehiculo);
@@ -42,60 +48,61 @@ function RegisterList({ registros, selectedVehicle }) {
       </ul>
       {currentSelectedVehicle && (
         <div className="vehicle-details container">
-          <h2 className="text-center font-weight-bold mb-4">Detalles de Vehículo</h2>
-          <div className="row detail-row">
-            <span className="col-6 font-weight-bold">Numero de servicio:</span>
-            <span className="col-6">{currentSelectedVehicle.servicioNumber}</span>
-          </div>
-          <div className="row detail-row">
-            <span className="col-6 font-weight-bold">Marca:</span>
-            <span className="col-6">{currentSelectedVehicle.marca}</span>
-          </div>
-          <div className="row detail-row">
-            <span className="col-6 font-weight-bold">Modelo:</span>
-            <span className="col-6">{currentSelectedVehicle.modelo}</span>
-          </div>
-          <div className="row detail-row">
-            <span className="col-6 font-weight-bold">Cliente:</span>
-            <span className="col-6">{currentSelectedVehicle.cliente}</span>
-          </div>
-          <div className="row detail-row">
-            <span className="col-6 font-weight-bold">Patente:</span>
-            <span className="col-6">{currentSelectedVehicle.patente}</span>
-          </div>
-          <div className="row detail-row">
-            <span className="col-6 font-weight-bold">Cilindrada:</span>
-            <span className="col-6">{currentSelectedVehicle.cilindrada}</span>
-          </div>
-          <div className="row detail-row">
-            <span className="col-6 font-weight-bold">Ultimo Cambio de Aceite:</span>
-            <span className="col-6">{currentSelectedVehicle.ultimoCambioAceite}</span>
-          </div>
-          <div className="row detail-row">
-            <span className="col-6 font-weight-bold">Kilometraje:</span>
-            <span className="col-6">{currentSelectedVehicle.kilometraje}</span>
-          </div>
-          <div className="row detail-row">
-            <span className="col-6 font-weight-bold">Color:</span>
-            <span className="col-6">{currentSelectedVehicle.color}</span>
-          </div>
-          <div className="sidebar-container">
-
-          </div>
+        <h2 className="text-center font-weight-bold mb-4">Detalles de Vehículo</h2>
+        <div className="row detail-row">
+          <span className="col-6 font-weight-bold">Numero de servicio:</span>
+          <span className="col-6">{currentSelectedVehicle.servicioNumber}</span>
         </div>
+        <div className="row detail-row">
+          <span className="col-6 font-weight-bold">Marca:</span>
+          <span className="col-6">{currentSelectedVehicle.marca}</span>
+        </div>
+        <div className="row detail-row">
+          <span className="col-6 font-weight-bold">Modelo:</span>
+          <span className="col-6">{currentSelectedVehicle.modelo}</span>
+        </div>
+        <div className="row detail-row">
+          <span className="col-6 font-weight-bold">Cliente:</span>
+          <span className="col-6">{currentSelectedVehicle.cliente}</span>
+        </div>
+        <div className="row detail-row">
+          <span className="col-6 font-weight-bold">Patente:</span>
+          <span className="col-6">{currentSelectedVehicle.patente}</span>
+        </div>
+        <div className="row detail-row">
+          <span className="col-6 font-weight-bold">Cilindrada:</span>
+          <span className="col-6">{currentSelectedVehicle.cilindrada}</span>
+        </div>
+        <div className="row detail-row">
+          <span className="col-6 font-weight-bold">Ultimo Cambio de Aceite:</span>
+          <span className="col-6">{currentSelectedVehicle.ultimoCambioAceite}</span>
+        </div>
+        <div className="row detail-row">
+          <span className="col-6 font-weight-bold">Kilometraje:</span>
+          <span className="col-6">{currentSelectedVehicle.kilometraje}</span>
+        </div>
+        <div className="row detail-row">
+          <span className="col-6 font-weight-bold">Color:</span>
+          <span className="col-6">{currentSelectedVehicle.color}</span>
+        </div>
+        <div className="jobs-container">
+            <h3>Trabajos Asociados</h3>
+            <ul>
+              {currentSelectedVehicle.trabajos && currentSelectedVehicle.trabajos.map((job, index) => (
+                <li key={index}>{job.descripcion}</li>
+              ))}
+            </ul>
+          </div>
+      </div>
       )}
       {currentSelectedVehicle && (
         <Requeriments
           selectedVehicle={currentSelectedVehicle}
-          jobs={currentSelectedVehicle.jobs || []}
+          jobs={currentSelectedVehicle.trabajos || []}
         />
       )}
 
-      {registros.length === 0 && (
-        <div>
-          <h3>Sin registros</h3>
-        </div>
-      )}
+      {registros.length === 0 && <div><h3>Sin registros</h3></div>}
     </div>
   );
 }
--- src/components/back.js
+++ src/components/back.js
@@ -1,36 +1,32 @@
 const express = require('express');
-const AWS = require('aws-sdk');
 const { DynamoDBClient, PutItemCommand, UpdateItemCommand, GetItemCommand, ScanCommand } = require('@aws-sdk/client-dynamodb');
 const { fromIni } = require('@aws-sdk/credential-provider-ini');
 const bodyParser = require('body-parser');
 const config = require('./config.json');
 const cors = require('cors');
+const AWS = require('aws-sdk');
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(cors());
-AWS.config.update({
-  region: config.region,
-  accessKeyId: config.accessKeyId,
-  secretAccessKey: config.secretAccessKey,
-});
+app.use(bodyParser.json());
 
 const dynamoDbClient = new DynamoDBClient({
   region: config.region,
   credentials: fromIni({ profile: 'Alan' }),
 });
 
-app.use(bodyParser.json());
-
 // Ruta para crear un nuevo registro de vehículo
 app.post('/api/vehiculos', async (req, res) => {
   try {
     const { marca, modelo, cliente, patente, cilindrada, anioFabricacion, ultimoCambioAceite, kilometraje, color } = req.body;
+    const servicioNumber = new Date().toISOString().replace(/[:.-]/g, '');
 
     const params = {
       TableName: 'Vehiculos',
       Item: {
-        servicioNumber: { S: new Date().toISOString().replace(/[:.-]/g, '') },
+        servicioNumber: { S: servicioNumber },
         marca: { S: marca },
         modelo: { S: modelo },
         cliente: { S: cliente },
@@ -40,24 +36,24 @@ app.post('/api/vehiculos', async (req, res) => {
         ultimoCambioAceite: { S: ultimoCambioAceite },
         kilometraje: { S: kilometraje },
         color: { S: color },
-        trabajos: { L: [] }, // Inicializa la lista de trabajos como vacía
+        trabajos: { L: [] },
       },
     };
 
     await dynamoDbClient.send(new PutItemCommand(params));
-    res.json({ message: 'Registro de vehículo creado con éxito' });
+    res.json({ message: 'Registro de vehículo creado con éxito', servicioNumber: servicioNumber });
   } catch (error) {
     console.error('Error al crear el registro de vehículo:', error);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
 
-// Ruta para crear un nuevo registro de trabajo y asociarlo a un vehículo
-app.post('/api/trabajos', async (req, res) => {
+// Ruta para agregar un trabajo a un vehículo existente
+app.post('/api/vehiculos/:servicioNumber/trabajos', async (req, res) => {
   try {
-    const { servicioNumber, descripcion } = req.body;
+    const { servicioNumber } = req.params;
+    const { descripcion } = req.body;
 
-    // Actualiza el registro de vehículo existente para agregar el trabajo
     const updateParams = {
       TableName: 'Vehiculos',
       Key: {
@@ -71,13 +67,13 @@ app.post('/api/trabajos', async (req, res) => {
         ':trabajo': { L: [{ S: descripcion }] },
         ':empty_list': { L: [] },
       },
-      ReturnValues: 'ALL_NEW',
+      ReturnValues: 'UPDATED_NEW',
     };
 
-    await dynamoDbClient.send(new UpdateItemCommand(updateParams));
-    res.json({ message: 'Registro de trabajo creado y asociado al vehículo con éxito' });
+    const result = await dynamoDbClient.send(new UpdateItemCommand(updateParams));
+    res.json({ message: 'Trabajo agregado al vehículo con éxito', updatedAttributes: result.Attributes });
   } catch (error) {
-    console.error('Error al crear el registro de trabajo y asociarlo al vehículo:', error);
+    console.error('Error al agregar el trabajo al vehículo:', error);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
@@ -90,24 +86,9 @@ app.get('/api/vehiculos', async (req, res) => {
     };
 
     const data = await dynamoDbClient.send(new ScanCommand(params));
-    res.json(data.Items);
+    res.json(data.Items.map(item => AWS.DynamoDB.Converter.unmarshall(item)));
   } catch (error) {
     console.error('Error al obtener la información de los vehículos:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
-});
-
-// Ruta para obtener información de todos los trabajos
-app.get('/api/trabajos', async (req, res) => {
-  try {
-    const params = {
-      TableName: 'Trabajos',
-    };
-
-    const data = await dynamoDbClient.send(new ScanCommand(params));
-    res.json(data.Items);
-  } catch (error) {
-    console.error('Error al obtener la información de los trabajos:', error);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
@@ -128,34 +109,10 @@ app.get('/api/vehiculos/:servicioNumber', async (req, res) => {
     if (!data.Item) {
       res.status(404).json({ error: 'Vehículo no encontrado' });
     } else {
-      res.json(data.Item);
+      res.json(AWS.DynamoDB.Converter.unmarshall(data.Item));
     }
   } catch (error) {
     console.error('Error al obtener la información del vehículo:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  }
-});
-
-// Ruta para obtener información de un trabajo específico por su servicioNumber
-app.get('/api/trabajos/:servicioNumber', async (req, res) => {
-  try {
-    const { servicioNumber } = req.params;
-
-    const params = {
-      TableName: 'Trabajos',
-      Key: {
-        servicioNumber: { S: servicioNumber },
-      },
-    };
-
-    const data = await dynamoDbClient.send(new GetItemCommand(params));
-    if (!data.Item) {
-      res.status(404).json({ error: 'Trabajo no encontrado' });
-    } else {
-      res.json(data.Item);
-    }
-  } catch (error) {
-    console.error('Error al obtener la información del trabajo:', error);
     res.status(500).json({ error: 'Error interno del servidor' });
   }
 });
